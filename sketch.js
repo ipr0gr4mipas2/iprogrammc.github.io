98d84ac9a95ab9eed04ed5d0cbae5f12d6fddcf5
iProgramMC <andrei.cluci.ac@gmail.com>
2018-08-28 15:35:10 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(300,300);
+  // put setup code here
+  // createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+  // put drawing code here
+  background(129,203,255);
+  triangle(0,0,231,231,0,231); // Should produce a right triangle
+}
